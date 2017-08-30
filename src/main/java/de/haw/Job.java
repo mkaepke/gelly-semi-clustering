fb77495703e74c09f4a9772feffe94a47eb03d70
mkaepke <ehK-sx7-kmJ-otg>
2017-08-30 11:21:56 +0200

    prepare methods now included of Semi-Clustering

--- src/main/java/de/haw/Job.java
+++ src/main/java/de/haw/Job.java
@@ -1,8 +1,5 @@
 package de.haw;
 
-import de.haw.PrepareHelper.EdgeNormalizer;
-import de.haw.PrepareHelper.LoopFilter;
-import de.haw.SemiHelper.SemiParams;
 import de.haw.SemiHelper.SemiVertexValue;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
@@ -15,6 +12,13 @@ import org.apache.flink.types.NullValue;
  * Example job for running the Semi-Clustering algorithm by a comma separated edge-list as input file
  *
  * e.g.: --input /Users/path/to/your/edge/list.txt --algo vc
+ *
+ * optional parameter:
+ * --m for max iteration (def.: 2)
+ * --i to set init score (def.: 1.0)
+ * --f to set score factor (def.: 0.5)
+ * --x for top X clusters (bests) (def.: 3)
+ * --c for SemiCluster capacity (def.: 2)
  */
 public class Job {
 
@@ -24,16 +28,6 @@ public class Job {
      * parse args
      */
     ParameterTool parameter = ParameterTool.fromArgs(args);
-
-    /**
-     * set values for Semi-Clustering by arguments or keep default
-     */
-    SemiParams semiParams = new SemiParams();
-    semiParams.setMaxIterations(parameter.getInt("m", 2));
-    semiParams.setInitScore(parameter.getDouble("i", 1.0));
-    semiParams.setFactor(parameter.getDouble("f", 0.5));
-    semiParams.setTopXOfClusters(parameter.getInt("x", 3));
-    semiParams.setClusterCapacity(parameter.getInt("c", 2));
 
     /**
      * set up the execution environment
@@ -51,29 +45,17 @@ public class Job {
         .types(Double.class, Double.class, Double.class);
 
     /**
-     * prepare input and optimize edges
-     */
-    DataSet<Tuple3<Double, Double, Double>> preparedEdgeTuple = edgeTuples
-        .filter(new LoopFilter())
-        .name("remove self loops")
-        .flatMap(new EdgeNormalizer())
-        .name("reverse edges => srcID < trgID")
-        .groupBy(0, 1)
-        .sum(2)
-        .name("combine multi edges");
-
-    /**
      * build input graph
      */
-    Graph<Double, NullValue, Double> inputGraph = Graph.fromTupleDataSet(preparedEdgeTuple, env);
+    Graph<Double, NullValue, Double> inputGraph = Graph.fromTupleDataSet(edgeTuples, env);
 
     /**
      * run Semi-Clustering
      */
     Graph<Double, SemiVertexValue, Double> result;
     switch(parameter.getRequired("algo")) {
-      case "sg" : result = inputGraph.run(new SemiClusteringSG(semiParams, env)); break;
-      case "vc" : result = inputGraph.run(new SemiClusteringVC(semiParams, env)); break;
+      case "sg" : result = inputGraph.run(new SemiClusteringSG(parameter, env)); break;
+      case "vc" : result = inputGraph.run(new SemiClusteringVC(parameter, env)); break;
 
       default : throw new IllegalArgumentException("choose: sg for scatter-gather or vc for vertex-centric");
     }
--- src/main/java/de/haw/SemiClusteringVC.java
+++ src/main/java/de/haw/SemiClusteringVC.java
@@ -1,6 +1,8 @@
 package de.haw;
 
+import de.haw.PrepareHelper.EdgeNormalizer;
 import de.haw.PrepareHelper.IncidentEdges;
+import de.haw.PrepareHelper.LoopFilter;
 import de.haw.SemiHelper.SemiParams;
 import de.haw.SemiHelper.SemiVertexValue;
 import de.haw.SemiVCHelper.SemiCombiner;
@@ -8,6 +10,8 @@ import de.haw.SemiVCHelper.SemiCompute;
 import java.io.Serializable;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.graph.EdgeDirection;
 import org.apache.flink.graph.Graph;
 import org.apache.flink.graph.GraphAlgorithm;
@@ -23,14 +27,36 @@ public class SemiClusteringVC implements GraphAlgorithm<Double, NullValue, Doubl
   private SemiParams semiParams;
   private ExecutionEnvironment env;
 
-  public SemiClusteringVC(SemiParams semiParams, ExecutionEnvironment env) {
-    this.semiParams = semiParams;
+  public SemiClusteringVC(ParameterTool parameter, ExecutionEnvironment env) {
+    this.semiParams = new SemiParams();
     this.env = env;
+
+    /**
+     * set values for Semi-Clustering by arguments or keep default
+     */
+    semiParams.setMaxIterations(parameter.getInt("m", 2));
+    semiParams.setInitScore(parameter.getDouble("i", 1.0));
+    semiParams.setFactor(parameter.getDouble("f", 0.5));
+    semiParams.setTopXOfClusters(parameter.getInt("x", 3));
+    semiParams.setClusterCapacity(parameter.getInt("c", 2));
   }
 
   @Override
   public Graph<Double, SemiVertexValue, Double> run(Graph<Double, NullValue, Double> input) throws Exception {
     Preconditions.checkArgument(semiParams.getMaxIterations() > 0, "max iteration must be greater than 0");
+
+    /**
+     * prepare input and optimize edges
+     */
+    DataSet<Tuple3<Double, Double, Double>> preparedEdgeTuple = input.getEdgesAsTuple3()
+        .filter(new LoopFilter())
+        .name("remove self loops")
+        .flatMap(new EdgeNormalizer())
+        .name("reverse edges => srcID < trgID")
+        .groupBy(0, 1)
+        .sum(2)
+        .name("combine multi edges");
+    input = Graph.fromTupleDataSet(preparedEdgeTuple, env);
 
     /**
      * prepare graph
